c361a30e9fad2f2a92c140a44421b8ee97c50882
unknown <ofeeki111@gmail.com>
2020-07-30 02:09:22 +0300

    first commit

---
+++ script-review.js
@@ -0,0 +1,14 @@
+var loadAnimation = bodymovin.loadAnimation({
+  container: loading,
+  renderer: 'svg',
+  loop:true,
+  autoplay: true,
+  path: 'data.json'
+})
+
+
+
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loading");
+  loader.className += " hidden"; // class "loader hidden"
+});
